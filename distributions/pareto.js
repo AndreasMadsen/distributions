e501a05acf6e464e57b2f0bc657eb5fd3915278d
Matt Hink <mhink1103@gmail.com>
2015-10-04 14:51:38 -0700

    Adding more methods for the Pareto distribution

--- distributions/pareto.js
+++ distributions/pareto.js
@@ -18,16 +18,47 @@ function ParetoDistribution(alpha, xm) {
 exports.ParetoDistribution = ParetoDistribution;
 
 ParetoDistribution.prototype.pdf = function(x) {
+  if (x < this._xm) {
+    throw TypeError("x must be greater than x_m.");
+  }
+
   return ((this._alpha * Math.pow(this._xm, this._alpha)) / Math.pow(x, (this._alpha + 1)));
 }
 
 ParetoDistribution.prototype.cdf = function(x) {
-  return (1 - Math.pow((xm / x), this._alpha));
+  if (x < this._xm) {
+    throw TypeError("x must be greater than x_m.");
+  }
+  return (1 - Math.pow((this._xm / x), this._alpha));
+};
+
+ParetoDistribution.prototype.inv = function(p) {
+  if (p < 0 || p >= 1) {
+    throw TypeError("p must fall within the range [0, 1)");
+  }
+
+  return this._xm / (Math.pow((1 - p), (1/this._alpha)));
+};
+
+ParetoDistribution.prototype.median = function() {
+  var log2BaseAlpha = Math.log(2) / Math.log(this._alpha);
+  return (this._xm * log2BaseAlpha);
 };
 
 ParetoDistribution.prototype.mean = function() {
-  if(this._alpha <= 1) 
+  if(this._alpha <= 1) {
     return Number.POSITIVE_INFINITY;
-  else 
+  } else {
     return (this._alpha * this._xm) / (this._alpha - 1);
+  }
+};
+
+ParetoDistribution.prototype.variance = function() {
+  if(this._alpha < 1 && this._alpha <= 2) {
+    return Number.POSITIVE_INFINITY;
+  }
+
+  var num   = Math.pow(this._xm, 2) * this._alpha;
+  var denom = Math.pow(this._alpha-1, 2) * (this._alpha - 2);
+  return (num/denom);
 };
